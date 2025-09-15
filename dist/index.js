acb1a89f4307707897b836ad61323464cf191078
Quynh Nguyen <seriquynh@gmail.com>
2025-09-15 13:16:17 +0700

    Install the hapideploy library

--- dist/index.js
+++ dist/index.js
@@ -27258,15 +27258,29 @@ async function run() {
     const user = coreExports.getInput('user');
     const identityKey = coreExports.getInput('identity_key');
 
+    // Create the identity_file file
     fs.writeFileSync('./identity_file', identityKey);
     fs.chmodSync('./identity_file', '600');
 
+    // create the inventory.yml file
     fs.writeFileSync('./inventory.yml', 'remotes:\n');
     fs.appendFileSync('./inventory.yml', '  app-server:\n');
     fs.appendFileSync('./inventory.yml', `    host: ${host}\n`);
     fs.appendFileSync('./inventory.yml', `    port: ${port}\n`);
     fs.appendFileSync('./inventory.yml', `    user: ${user}\n`);
     fs.appendFileSync('./inventory.yml', `    identity_file: ./identity_file\n`);
+
+    // Install the hapideploy library
+    // TODO: Check if python & pip are available.
+    const child = require$$2$2.spawn('pip install hapideploy');
+
+    child.stdout.on('data', (data) => {
+      console.log(`${data}\n`);
+    });
+
+    child.stderr.on('data', (data) => {
+      console.error(`${data}\n`);
+    });
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) coreExports.setFailed(error.message);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import fs from 'fs'
+import cp from 'child_process'
 
 /**
  * The main function for the action.
@@ -13,15 +14,29 @@ export async function run() {
     const user = core.getInput('user')
     const identityKey = core.getInput('identity_key')
 
+    // Create the identity_file file
     fs.writeFileSync('./identity_file', identityKey)
     fs.chmodSync('./identity_file', '600')
 
+    // create the inventory.yml file
     fs.writeFileSync('./inventory.yml', 'remotes:\n')
     fs.appendFileSync('./inventory.yml', '  app-server:\n')
     fs.appendFileSync('./inventory.yml', `    host: ${host}\n`)
     fs.appendFileSync('./inventory.yml', `    port: ${port}\n`)
     fs.appendFileSync('./inventory.yml', `    user: ${user}\n`)
     fs.appendFileSync('./inventory.yml', `    identity_file: ./identity_file\n`)
+
+    // Install the hapideploy library
+    // TODO: Check if python & pip are available.
+    const child = cp.spawn('pip install hapideploy')
+
+    child.stdout.on('data', (data) => {
+      console.log(`${data}\n`)
+    })
+
+    child.stderr.on('data', (data) => {
+      console.error(`${data}\n`)
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
